ee2aafef71a91afc1b76790f7724df6d13fcdb54
Nicolas Galipot <nicolas.galipot@gmail.com>
2024-08-11 22:15:37 +0200

    Make the C snake version code closer to the Rust and C++ versions

--- src/snake.c
+++ src/snake.c
@@ -16,42 +16,37 @@ extern int32_t js_random(int32_t max);
 #define GRID_WIDTH 40
 #define GRID_HEIGHT 40
 
-enum Direction
-{
+typedef enum {
     DIRECTION_UP,
     DIRECTION_DOWN,
     DIRECTION_LEFT,
     DIRECTION_RIGHT
-};
-enum KeyCode
-{
+} Direction;
+
+typedef enum {
     KEY_CODE_ARROW_UP,
     KEY_CODE_ARROW_DOWN,
     KEY_CODE_ARROW_LEFT,
     KEY_CODE_ARROW_RIGHT
-};
+} KeyCode;
 
-bool direction_is_opposite(enum Direction dir, enum Direction other)
-{
+bool direction_is_opposite(Direction dir, Direction other) {
     return dir == DIRECTION_UP && other == DIRECTION_DOWN ||
            dir == DIRECTION_DOWN && other == DIRECTION_UP ||
            dir == DIRECTION_LEFT && other == DIRECTION_RIGHT ||
            dir == DIRECTION_RIGHT && other == DIRECTION_LEFT;
 }
 
-struct Position
-{
+typedef struct {
     int32_t x;
     int32_t y;
-};
+} Position;
 
-bool position_equals(struct Position pos, struct Position other)
-{
+bool position_equals(Position pos, Position other) {
     return pos.x == other.x && pos.y == other.y;
 }
 
-struct Position position_moved(struct Position pos, enum Direction direction)
-{
+Position position_moved(Position pos, Direction direction) {
     switch (direction) {
     case DIRECTION_UP:
         pos.y--;
@@ -69,26 +64,22 @@ struct Position position_moved(struct Position pos, enum Direction direction)
     return pos;
 }
 
-struct Snake
-{
-    struct Position segments[GRID_WIDTH * GRID_HEIGHT];
+typedef struct {
+    Position segments[GRID_WIDTH * GRID_HEIGHT];
     int32_t length;
     int32_t head_index;
-    enum Direction direction;
-};
+    Direction direction;
+} Snake;
 
-struct Position snake_head_position(struct Snake *snake)
-{
+Position snake_head_position(Snake *snake) {
     return snake->segments[snake->head_index];
 }
 
-struct Position snake_next_head_position(struct Snake *snake)
-{
+Position snake_next_head_position(Snake *snake) {
     return position_moved(snake->segments[snake->head_index], snake->direction);
 }
 
-bool snake_eats_himself(struct Snake *snake)
-{
+bool snake_eats_himself(Snake *snake) {
     for (int32_t i = 0; i < snake->length; i++) {
         if (i == snake->head_index) {
             continue;
@@ -100,18 +91,16 @@ bool snake_eats_himself(struct Snake *snake)
     return false;
 }
 
-bool snake_is_out_of_bounds(struct Snake *snake, int32_t width, int32_t height)
-{
-    struct Position headPosition = snake_head_position(snake);
+bool snake_is_out_of_bounds(Snake *snake, int32_t width, int32_t height) {
+    Position headPosition = snake_head_position(snake);
     return headPosition.x < 0 ||
            headPosition.x >= width ||
            headPosition.y < 0 ||
            headPosition.y >= height;
 }
 
-void snake_move_ahead(struct Snake *snake)
-{
-    struct Position nextHeadPosition = snake_next_head_position(snake);
+void snake_move_ahead(Snake *snake) {
+    Position nextHeadPosition = snake_next_head_position(snake);
     if (snake->head_index == snake->length - 1) {
         snake->head_index = 0;
     } else {
@@ -120,9 +109,8 @@ void snake_move_ahead(struct Snake *snake)
     snake->segments[snake->head_index] = nextHeadPosition;
 }
 
-void snake_grow(struct Snake *snake)
-{
-    struct Position nextHeadPosition = snake_next_head_position(snake);
+void snake_grow(Snake *snake) {
+    Position nextHeadPosition = snake_next_head_position(snake);
     if (snake->head_index == snake->length) {
         snake->segments[snake->length] = nextHeadPosition;
     } else {
@@ -134,120 +122,113 @@ void snake_grow(struct Snake *snake)
     snake->length++;
 }
 
-void paint_background(void)
-{
+void paint_background(void) {
     canvas_set_fill_style(COLOR_BACKGROUND);
     canvas_fill_rect(0, 0, GRID_WIDTH * CELL_SIZE, GRID_HEIGHT * CELL_SIZE);
 }
 
-void paint_snake(struct Snake *snake)
-{
+void paint_snake(Snake *snake) {
     canvas_set_fill_style(COLOR_SNAKE);
     for (int i = 0; i < snake->length; i++) {
-        struct Position segment = snake->segments[i];
+        Position segment = snake->segments[i];
         canvas_fill_rect(segment.x * CELL_SIZE, segment.y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
     }
 }
 
-void paint_apple(struct Position apple)
-{
+void paint_apple(Position apple) {
     canvas_set_fill_style(COLOR_APPLE);
     canvas_fill_rect(apple.x * CELL_SIZE, apple.y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
 }
 
-static struct Snake snake;
-static struct Position apple;
-static int32_t stepPeriod;
-static int32_t score;
-static int32_t next_reward;
+typedef struct {
+    Snake snake;
+    Position apple;
+    int32_t stepPeriod;
+    int32_t score;
+    int32_t next_reward;
+} GameState;
 
-void change_snake_direction(enum Direction d) 
-{
-    if (direction_is_opposite(snake.direction, d)) {
+static GameState GAME_STATE;
+
+
+void change_snake_direction(GameState *state, Direction d) {
+    if (direction_is_opposite(state->snake.direction, d)) {
         return;
     }
-    snake.direction = d;
+    state->snake.direction = d;
 }
 
-void on_key_down(enum KeyCode code)
-{
+void on_key_down(KeyCode code) {
     switch (code) {
     case KEY_CODE_ARROW_UP:
-        change_snake_direction(DIRECTION_UP);
+        change_snake_direction(&GAME_STATE, DIRECTION_UP);
         break;
     case KEY_CODE_ARROW_DOWN:
-        change_snake_direction(DIRECTION_DOWN);
+        change_snake_direction(&GAME_STATE, DIRECTION_DOWN);
         break;
     case KEY_CODE_ARROW_LEFT:
-        change_snake_direction(DIRECTION_LEFT);
+        change_snake_direction(&GAME_STATE, DIRECTION_LEFT);
         break;
     case KEY_CODE_ARROW_RIGHT:
-        change_snake_direction(DIRECTION_RIGHT);
+        change_snake_direction(&GAME_STATE, DIRECTION_RIGHT);
         break;
     }
 }
 
-void speedup_game(void)
-{
-    if (stepPeriod > 50) {
-        stepPeriod -= 25;
-        snake_step_period_updated(stepPeriod);
+void speedup_game(GameState *state) {
+    if (state->stepPeriod > 50) {
+        state->stepPeriod -= 25;
+        snake_step_period_updated(state->stepPeriod);
     }
 }
 
-bool snake_will_eat_apple(void) 
-{ 
-    return position_equals(snake_next_head_position(&snake), apple);
+bool snake_will_eat_apple(GameState *state) { 
+    return position_equals(snake_next_head_position(&state->snake), state->apple);
 }
 
-void update_score(void)
-{
-    score += next_reward;
-    next_reward += 10;
+void update_score(GameState *state) {
+    state->score += state->next_reward;
+    state->next_reward += 10;
 }
 
-void teleport_apple(void)
-{
-    apple.x = js_random(GRID_WIDTH);
-    apple.y = js_random(GRID_HEIGHT);
+void teleport_apple(GameState *state) {
+    state->apple.x = js_random(GRID_WIDTH);
+    state->apple.y = js_random(GRID_HEIGHT);
 }
 
-void repaint(void)
-{
+void repaint(GameState *state) {
     paint_background();
-    paint_snake(&snake);
-    paint_apple(apple);
+    paint_snake(&state->snake);
+    paint_apple(state->apple);
     canvas_fill();
 }
 
-void step(int32_t timestamp)
-{
-    if (snake_will_eat_apple()) {
-        snake_grow(&snake);
-        teleport_apple();
-        speedup_game();
-        update_score();
-        snake_score_changed(score);
+void step(int32_t timestamp) {
+    if (snake_will_eat_apple(&GAME_STATE)) {
+        snake_grow(&GAME_STATE.snake);
+        teleport_apple(&GAME_STATE);
+        speedup_game(&GAME_STATE);
+        update_score(&GAME_STATE);
+        snake_score_changed(GAME_STATE.score);
     } else {
-        snake_move_ahead(&snake);
+        snake_move_ahead(&GAME_STATE.snake);
     }
-    if (snake_is_out_of_bounds(&snake, GRID_WIDTH, GRID_HEIGHT) || snake_eats_himself(&snake)) {
+    if (snake_is_out_of_bounds(&GAME_STATE.snake, GRID_WIDTH, GRID_HEIGHT) || snake_eats_himself(&GAME_STATE.snake)) {
         snake_game_over();
     }
-    repaint();
+    repaint(&GAME_STATE);
 }
 
-void init()
-{
-    stepPeriod = 300;
-    next_reward = 10;
-    teleport_apple();
-    snake.length = 4;
-    snake.head_index = 3;
-    snake.direction = DIRECTION_RIGHT;
-    snake.segments[1].x = 1;
-    snake.segments[2].x = 2;
-    snake.segments[3].x = 3;
-    repaint();
+void init() {
+    GAME_STATE.stepPeriod = 300;
+    GAME_STATE.next_reward = 10;
+    teleport_apple(&GAME_STATE);
+    GAME_STATE.snake.length = 4;
+    GAME_STATE.snake.head_index = 3;
+    GAME_STATE.snake.direction = DIRECTION_RIGHT;
+    GAME_STATE.snake.segments[1].x = 1;
+    GAME_STATE.snake.segments[2].x = 2;
+    GAME_STATE.snake.segments[3].x = 3;
+    repaint(&GAME_STATE);
     snake_score_changed(0);
 }
